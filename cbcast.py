55672bb0ed46a791aa2641d219baa601fd6a443c
Yuki Yamada <y.yamada6426@gmail.com>
2018-01-23 15:53:30 +0900

    cbcastのバグ修正

--- cbcast.py
+++ cbcast.py
@@ -2,17 +2,23 @@ import hashlib
 import json
 
 import requests
+from pymongo import MongoClient
+
+mongoClient = MongoClient('localhost', 27017)
+db = mongoClient.gc_blockchain
 
 
 class CBCast(object):
     def __init__(self, myId):
-        self.count = {myId: 0}
+        self.count = db.cbcast_count
+        self.count.insert({"id": myId, "count": 0})
         self.myId = myId
         self.myip = ""
         self.vector_key = "cbcast_vector"
         self.sender_id = "sender_id"
-        self.txqueue = []
+        self.txqueue = db.cbcast_queue
         self.timeStampSync = False
+        self.callback = None
 
     def objectHash(self, dic):
         return hashlib.md5(str(dic).encode('utf-8')).hexdigest()
@@ -22,14 +28,14 @@ class CBCast(object):
         received = set(values.get("received", []))
         received.add(self.myip)
         values["received"] = list(received)
-        self.count[self.myId] += additional
+        self.count.update({"id": self.myId}, {"$inc": {"count": additional}})
         print(f"received: {list(received)}")
         # print(f"countupdated: {self.count[self.myId]}")
         for node in set(nodeList) - received:
             self.send(f"http://{node}/{func}", values)
 
     def send(self, url, param={}):
-        param[self.vector_key] = self.count
+        param[self.vector_key] = list(self.count.find({}, {'_id': 0}))
         param[self.sender_id] = self.myId
         print(f"sender: {param}")
         return requests.post(url, data=json.dumps(param), headers={"content-type": "application/json"})
@@ -38,21 +44,25 @@ class CBCast(object):
         sender_id = param.get(self.sender_id, None)
         if sender_id is None:
             return callback(param)
-        messageVector = param.get(self.vector_key, {})
-        ret = self.checkReceivable(self.count, messageVector, sender_id)
+        if callback != "dummy":
+            self.callback = callback
+        #messageVector = param.get(self.vector_key, {})
+        messageVector = {i["id"]: i["count"] for i in param.get(self.vector_key, [])}
+        memoryVector = {i["id"]: i["count"] for i in list(self.count.find({}, {'_id': 0}))}
+        ret = self.checkReceivable(memoryVector, messageVector, sender_id)
         # print(f"receivable: {ret}")
         if ret:
             self.updateMemoryVector(sender_id, messageVector)
             print(f"[ACCEPT] {messageVector[sender_id]}")
-            tempQueue = self.txqueue
-            self.txqueue = []
+            tempQueue = self.txqueue.find({}, {'_id': 0})
+            self.txqueue.remove()
             for q in tempQueue:
                 self.receive(q["param"], q["callback"])
             return callback(param)
         else:
             # キューに貯めて受信可能になるのを待つ
             print(f"[REJECT] {messageVector[sender_id]}")
-            self.txqueue.append({"param": param, "callback": callback})
+            self.txqueue.insert({"param": param, "callback": "dummy"})
             return callback(param)
 
     def checkReceivable(self, memoryVector, messageVector, senderId):
@@ -73,5 +83,9 @@ class CBCast(object):
 
     # 正しくメッセージが受信された時、正しく受信された最後の番号を入れる
     def updateMemoryVector(self, senderId, messageVector):
-        self.count[senderId] = messageVector.get(senderId, 0)
+        count = messageVector.get(senderId, 0)
+        if self.count.find({"id": senderId}).count() > 0:
+            self.count.update({"id": senderId}, {"id": senderId, "count": count})
+        else:
+            self.count.insert({"id": senderId, "count": count})
         # print(f"count updated: {self.count[senderId]}")
